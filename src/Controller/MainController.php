1153f6ae6214d332ca7242b4a3be1b974833e508
Baron78x <nico78nb@gmail.com>
2022-04-29 15:11:27 +0200

    avant challenge

---
+++ src/Controller/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\Movies;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * Affiche la page d'accueil
+     * 
+     * @return Response
+     * @Route("/", name="home", methods={"GET"})
+     */
+    public function home(): Response
+    {
+        // on récupère les données depuis le modèle
+        $moviesModel = new Movies();
+        $moviesList = $moviesModel->getAllMovies();
+
+        return $this->render('main/home.html.twig', [
+            'moviesList' => $moviesList
+        ]);
+    }
+
+}
